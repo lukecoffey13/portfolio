553f1d7ddba94f522e12d619ca5085879656fae2
Luke Coffey <luke.m.coffey@gmail.com>
2022-04-16 12:42:59 +1000

    Made super basic updates to skeletion including cool text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./intro.css"
 
 const Intro = () => {
   return (
-    <div>
-        this is the intro
+    <div className="i">
+        <div className="i-left">
+            <div className="i-left-wrapper">
+                <h2 className="i-intro">Hello, my name is</h2>
+                <h1 className="i-name">Luke Coffey</h1>
+                <div className="i-title">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">Web Developer</div>
+                        <div className="i-title-item">UI/UX Designer</div>
+                        <div className="i-title-item">Photographer</div>
+                        <div className="i-title-item">Writer</div>
+                        <div className="i-title-item">Content Creator</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="i-right">right</div>
     </div>
   )
 }
