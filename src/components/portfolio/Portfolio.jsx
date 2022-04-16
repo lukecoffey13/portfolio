aafb465a18213e3f54eb005c179bfc48f2b5ce09
Luke Coffey <luke.m.coffey@gmail.com>
2022-04-16 18:50:21 +1000

    Basic portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,53 @@ import "./portfolio.scss"
 const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
-      
+      <h1>
+        Portfolio
+      </h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   )
 }
