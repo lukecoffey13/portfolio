82415aacdce07e5a8970e345ed0e2ed5a7ae27c7
Luke Coffey <luke.m.coffey@gmail.com>
2022-04-16 21:09:24 +1000

    Made majority of portfolio section with data - great use of lists and stuff

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,55 +1,88 @@
 import React from 'react'
+import PortfolioList from '../portfolioList/PortfolioList'
 import "./portfolio.scss"
+import { useEffect, useState } from "react";
+import {
+  featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio
+} from "../../data";
 
 const Portfolio = () => {
+
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+        id: "featured",
+        title: "Featured",
+    },
+    {
+        id: "web",
+        title: "Web App",
+    },
+    {
+        id: "mobile",
+        title: "Mobile App",
+    },
+    {
+        id: "design",
+        title: "Design",
+    },
+    {
+        id: "content",
+        title: "Content",
+    },
+];
+
+useEffect(() => {
+
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+
+  }, [selected])
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>
         Portfolio
       </h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
+        {list.map(item=>(
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
+        {data.map((d) => (
+          <div className="item">
           <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
+            src={d.img}
             alt=""
           />
-          <h3>Banking App</h3>
+          <h3>{d.title}</h3>
         </div>
-        <div className="item">
-        <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
-            alt=""
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-        <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
-            alt=""
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-        <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
-            alt=""
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-        <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.backerkit.com%2Fprojects%2Ftimechi-your-smart-productivity-tool&psig=AOvVaw0UDy9gB2Eo3i8aAMtyiMqy&ust=1650185222299000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMC_nJOZmPcCFQAAAAAdAAAAABAb"
-            alt=""
-          />
-          <h3>Banking App</h3>
-        </div>
+        ))}
+        
       </div>
     </div>
   )
